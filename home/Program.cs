52a03873ac7bc4a6153b088ae4ccb1652360f72a
Niky1992 <138252944+Niky1992@users.noreply.github.com>
2023-07-12 00:06:51 +0300

    home

---
+++ home/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// 456 -> 5 
+// 782 -> 8 
+// 918 -> 1 
+ 
+int Promt(string message)  
+{  
+    Console.Write(message);  
+    return Convert.ToInt32(Console.ReadLine());  
+}  
+  
+int number = Promt("Введите трёхзначное число: ");  
+  
+if(number >= 100 & number < 1000)  
+{ 
+     int mid = (number % 100 - number % 10) /10;  
+    Console.WriteLine($"Вторая цифра числа {number} является {mid}"); 
+} 
+else 
+{  
+    Console.WriteLine("Число не является трёхзначным");  
+}
